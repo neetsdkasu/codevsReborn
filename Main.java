b0c9182627180cb1947d10eafc27f374a9ee7424
Leonardone <neetsdkasu@gmail.com>
2019-05-09 06:47:45 +0900

    パック投下シミュレーション実装してスコアベストに落とすようにした

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 interface AI {
     String getName();
@@ -67,19 +68,62 @@ class State {
     static State scan(BufferedReader in) throws Exception {
         State state = new State();
         state.timeLeft = Long.parseLong(in.readLine());
-        state.stock = Long.parseLong(in.readLine());
+        state.stock = Integer.parseInt(in.readLine());
         state.gauge = Integer.parseInt(in.readLine());
-        state.score = Long.parseLong(in.readLine());
+        state.score = Integer.parseInt(in.readLine());
         state.field = Field.scan(in);
         return state;
     }
 
-    long timeLeft, stock, score;
-    int gauge;
+    static final int[] CHAIN_SCORES = new int[50];
+    static {
+        double s = 1.0;
+        for (int i = 1; i < CHAIN_SCORES.length; ++i) {
+            s *= 1.3;
+            CHAIN_SCORES[i] = CHAIN_SCORES[i - 1] + (int)Math.floor(s);
+        }
+    }
+
+    long timeLeft;
+    int stock, gauge, score;
     Field field;
 
     private State() {}
 
+    boolean isGameOver() {
+        return field.isDead();
+    }
+
+    State getCopy() {
+        State state = new State();
+        state.timeLeft = timeLeft;
+        state.stock = stock;
+        state.gauge = gauge;
+        state.score = score;
+        state.field = field.getCopy();
+        return state;
+    }
+
+    int putPack(Pack pack) {
+        field.putPack(pack);
+        int chain = 0;
+        for (;;) {
+            if (!field.erase()) {
+                break;
+            }
+            ++chain;
+            if (!field.drop()) {
+                break;
+            }
+        }
+        int add = CHAIN_SCORES[chain];
+        stock -= add / 2;
+        score += add;
+        if (chain > 0) {
+            gauge += 8;
+        }
+        return chain;
+    }
 }
 
 class Field {
@@ -94,9 +138,15 @@ class Field {
                 if (b == 5) {
                     field.fives++;
                 }
+                // if (b > 0 && field.tops[i] == 0) {
+                    // field.tops[i] = i + OFF_Y;
+                // }
+                if (b > 0 && field.top == 0) {
+                    field.top = i + OFF_Y;
+                }
             }
         }
-        for (int i = 0; i < WIDTH_EX; i++) {
+        for (int i = 0; i < WIDTH_EX; ++i) {
             field.cell[(HEIGHT_EX - 1) * WIDTH_EX + i] = OJAMA_CELL;
         }
         in.readLine(); // discard END
@@ -112,8 +162,21 @@ class Field {
     static final int HEIGHT_EX = HEIGHT + 4;
     static final int OFF_X = 1;
     static final int OFF_Y = 3;
+    static final int OFF_DEAD_Y = 2;
+    static final int FIELD_SIZE = WIDTH_EX * HEIGHT_EX;
 
-    int[] cell = new int[WIDTH_EX * HEIGHT_EX];
+    static final int[] DT = {
+        -WIDTH_EX - 1, -WIDTH_EX, -WIDTH_EX + 1,
+        -1, 1,
+        WIDTH_EX - 1, WIDTH_EX, WIDTH_EX + 1
+    };
+    static final int[] ERDT = {
+        -WIDTH_EX, -WIDTH_EX + 1, 1, WIDTH_EX + 1
+    };
+
+    int[] cell = new int[FIELD_SIZE];
+    // int[] tops = new int[WIDTH_EX];
+    int top = 0;
     int fives = 0;
 
     private Field() {}
@@ -128,6 +191,85 @@ class Field {
 
     boolean containsFive() {
         return fives > 0;
+    }
+
+    boolean isDead() {
+        for (int idx = OFF_DEAD_Y * WIDTH_EX + OFF_X, d = 0; d < WIDTH; ++d) {
+            if (cell[idx + d] != EMPTY_CELL) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    Field getCopy() {
+        Field field = new Field();
+        field.cell = Arrays.copyOf(cell, cell.length);
+        // field.tops = Arrays.copyOf(tops, tops.length);
+        field.top = top;
+        field.fives = fives;
+        return field;
+    }
+
+    void putPack(Pack pack) {
+        for (int x = 0; x < 2; ++x) {
+            int idx = (HEIGHT_EX - 2) * WIDTH_EX + (pack.pos + OFF_X + x);
+            while (cell[idx] > 0) { idx -= WIDTH_EX; }
+            for (int y = 2; y >= 0; y -= 2) {
+                int n = pack.get(y + x);
+                if (n != 0) {
+                    cell[idx] = n;
+                    top = Math.min(top, idx / WIDTH_EX);
+                    idx -= WIDTH_EX;
+                }
+            }
+        }
+    }
+
+    boolean erase() {
+        boolean[] flag = new boolean[cell.length];
+        for (int idx = (HEIGHT_EX - 1) * WIDTH_EX - 2; idx > WIDTH_EX; --idx) {
+            int c = cell[idx];
+            for (int dt : ERDT) {
+                if (c + cell[idx + dt] == 10) {
+                    flag[idx] = true;
+                    flag[idx + dt] = true;
+                }
+            }
+        }
+        boolean erased = false;
+        for (int i = 0; i < flag.length; ++i) {
+            if (flag[i]) {
+                if (cell[i] == 5) {
+                    --fives;
+                }
+                cell[i] = 0;
+                erased = true;
+            }
+        }
+        return erased;
+    }
+
+    boolean drop() {
+        boolean droped = false;
+        int top = HEIGHT_EX;
+        for (int x = OFF_X; x <= WIDTH; ++x) {
+            for (int idx = (HEIGHT_EX - 2) * WIDTH_EX + x; idx >= WIDTH_EX; idx -= WIDTH_EX) {
+                if (cell[idx] != 0) { continue; }
+                int emp = idx;
+                for (idx -= WIDTH_EX; idx > 0; idx -= WIDTH_EX) {
+                    if (cell[idx] == 0) { continue; }
+                    cell[emp] = cell[idx];
+                    cell[idx] = 0;
+                    emp -= WIDTH_EX;
+                    droped = true;
+                }
+                top = Math.min(top, emp / WIDTH_EX + 1);
+                break;
+            }
+        }
+        this.top = top;
+        return droped;
     }
 }
 
@@ -155,7 +297,7 @@ class Pack implements Command {
                 ROLLED_INDEXES[0][dy * SIZE + dx] = dy * SIZE + dx;
             }
         }
-        for (int i = 1; i < 4; i++) {
+        for (int i = 1; i < 4; ++i) {
             for (int dy = 0; dy < SIZE; ++dy) {
                 for (int dx = 0; dx < SIZE; ++dx) {
                     ROLLED_INDEXES[i][dx * SIZE + (SIZE - 1 - dy)] =
@@ -175,6 +317,10 @@ class Pack implements Command {
         return block[ROLLED_INDEXES[rot][i]];
     }
 
+    int get(int y, int x) {
+        return get(y * SIZE + x);
+    }
+
     void rotate() {
         rot = (rot + 1) & 3;
     }
@@ -187,6 +333,7 @@ class Pack implements Command {
 
 interface Skill extends Command {
     boolean canFire(State state);
+    void fire(State state);
 }
 
 class Bomb implements Skill {
@@ -199,6 +346,10 @@ class Bomb implements Skill {
         }
         return state.field.containsFive();
     }
+    public void fire(State state) {
+
+    }
+
     @Override
     public String toString() {
         return "S";
@@ -229,28 +380,60 @@ class MyAI implements AI {
             return Bomb.BOMB;
         }
 
-        int[] ys = new int[Field.WIDTH];
-        for (int x = 0; x < Field.WIDTH; ++x) {
-            for (int y = Field.HEIGHT - 1; y > 0; --y) {
-                if (field.isEmpty(y, x)) {
-                    ys[x] = y;
-                    break;
+        int bestRot = 0;
+        int bestX = 0;
+        int bestTop = 0;
+        long bestScore = 0;
+        for (int rot = 0; rot < 4; ++rot) {
+            pack.rot = rot;
+            for (int x = 0; x <= 8; ++x) {
+                pack.pos = x;
+                State tmp = my.getCopy();
+                tmp.putPack(pack);
+                if (tmp.isGameOver()) {
+                    continue;
+                }
+                if (tmp.score > bestScore ||
+                    (tmp.score == bestScore &&
+                        tmp.field.top > bestTop)) {
+                    bestRot = rot;
+                    bestX = x;
+                    bestTop = tmp.field.top;
+                    bestScore = tmp.score;
                 }
             }
         }
-        int bestX = 0;
-        int bestY = 0;
-        for (int x = 0; x <= 8; x++) {
-            int y = Math.min(ys[x], ys[x + 1]) * 100 + Math.max(ys[x], ys[x + 1]);
-            if (y > bestY) {
-                bestX = x;
-                bestY = y;
-            }
-        }
 
+        pack.rot = bestRot;
         pack.pos = bestX;
 
         return pack;
     }
+
+    void printP(Pack pack) {
+        for (int y = 0; y < Pack.SIZE; ++y) {
+            for (int x = 0; x < pack.pos; ++x) {
+                err.print("  ");
+            }
+            for (int x = 0; x < Pack.SIZE; ++x) {
+                err.printf("%d ", pack.get(y, x));
+            }
+            err.println();
+        }
+    }
+
+    void printF(Field field) {
+        for (int y = 0; y < Field.HEIGHT; ++y) {
+            for (int x = 0; x < Field.WIDTH; ++x) {
+                err.printf("%d ", field.get(y, x));
+            }
+            err.println();
+        }
+    }
+
+    void printL() {
+        err.println("---------------");
+    }
+
 }
 
